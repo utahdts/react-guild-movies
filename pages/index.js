cfe4e0beecf0cb5ebcb1ed85af035460d5574a6e
ahaws <ahaws@utah.gov>
2021-07-13 14:01:35 -0600

    Add more last minute edits
    Co-authored-by: vmilukdoh <vmilukdoh@users.noreply.github.com>
    Co-authored-by: Joseph Sharp <jmsharp@utah.gov>
    Co-authored-by: steveoh <sgourley@utah.gov>
    Co-authored-by: bemeibos <bemeibos@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
 import Head from 'next/head'
 import { useState } from 'react';
-import styles from '../styles/Home.module.css'
 
 const movies = [{}];
 export default function Home() {
   const [searchText, setSearchText] = useState('');
   const search = () => console.log(searchText);
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>React Guild Movies</title>
         <meta name="description" content="Recordings from React Guild presentations" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="p-4">
+        <h1 className="text-3xl text-center font-bold">
             React Guild Movies
         </h1>
         <div>
           <input
-            className="mt-0 text-gray-500 block w-full border-0 border-b-2 border-gray-300"
+            className="mt-0 text-gray-500 block w-1/2 border-0 border-b-2 border-gray-300"
             type="text"
             value={searchText}
             onChange={e => setSearchText(e.target.value)}
