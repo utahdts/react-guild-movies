73bf8f3d5a3d32e336c0a798a58d8001d8d061f3
ahaws <ahaws@utah.gov>
2021-08-31 13:53:47 -0600

    Add react-query and use in movie editor
    Co-authored-by: bemeibos <bemeibos@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import '../styles/globals.css';
+import {ReactQueryDevtools} from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        }
+    }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools/>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    )
 }
 
 export default MyApp
--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,96 +1,105 @@
 import { useState, useEffect } from 'react';
 import PropTypes from "prop-types";
+import { useQuery } from 'react-query';
+import produce from 'immer';
 
 const propTypes = {
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
 };
 const defaultProps = {
 };
 
 export default function Movie({ id: movieId }) {
+    const { isLoading, error, data: movieFromQuery } = useQuery(
+        ['fetchSingleMovie', movieId],
+        () => fetch(`/api/movie/${movieId}`).then(res => res.json())
+    );
     const [movie, setMovie] = useState(null);
-
-    // TODO: Move useEffect to a hook
     useEffect(() => {
-        fetch(`/api/movie/${movieId}`, { method: "get" })
-            .then(data => data.json())
-            .then(setMovie);
-    }, []);
+        setMovie(movieFromQuery);
+    }, [movieFromQuery]);
 
-    const changeField = field => e => setMovie({ [field]: e.target.value });
+    const changeField = field => e => setMovie(produce(draft => {
+        draft[field] = e.target.value;
+    }));
 
     return (
-        <form className="w-1/2 p-10 mx-auto" action="#" method="POST">
-            <div className="mb-4">
-                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
-                    Name
-                </label>
-                <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    value={movie?.title}
-                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={changeField('title')}
-                />
-            </div>
+        movie ? (
+            <form className="w-1/2 p-10 mx-auto" action="#" method="POST">
+                <div className="mb-4">
+                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        value={movie?.title ?? ''}
+                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={changeField('title')}
+                    />
+                </div>
 
-            <div className="mb-4">
-                <label htmlFor="date" className="block text-sm font-medium text-gray-700">
-                    Date
-                </label>
-                <input
-                    type="text"
-                    name="date"
-                    id="date"
-                    value={movie?.date}
-                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={changeField('date')}
-                />
-            </div>
+                <div className="mb-4">
+                    <label htmlFor="date" className="block text-sm font-medium text-gray-700">
+                        Date
+                    </label>
+                    <input
+                        type="text"
+                        name="date"
+                        id="date"
+                        value={movie?.date ?? ''}
+                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={changeField('date')}
+                    />
+                </div>
 
-            <div className="mb-4">
-                <label htmlFor="presenter" className="block text-sm font-medium text-gray-700">
-                    Presenter
-                </label>
-                <input
-                    type="text"
-                    name="presenter"
-                    id="presenter"
-                    value={movie?.authors?.join(', ')}
-                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={changeField('authors')}
-                />
-            </div>
+                <div className="mb-4">
+                    <label htmlFor="presenter" className="block text-sm font-medium text-gray-700">
+                        Presenter
+                    </label>
+                    <input
+                        type="text"
+                        name="presenter"
+                        id="presenter"
+                        value={movie?.authors?.join(', ') ?? ''}
+                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={changeField('authors')}
+                    />
+                </div>
 
-            <div className="mb-4">
-                <label htmlFor="tags" className="block text-sm font-medium text-gray-700">
-                    Tags
-                </label>
-                <input
-                    type="text"
-                    name="tags"
-                    id="tags"
-                    value={movie?.tags?.join(', ')}
-                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={changeField('tags')}
-                />
-            </div>
+                <div className="mb-4">
+                    <label htmlFor="tags" className="block text-sm font-medium text-gray-700">
+                        Tags
+                    </label>
+                    {/* TODO: Change to an array picker */}
+                    <input
+                        type="text"
+                        name="tags"
+                        id="tags"
+                        value={movie?.tags?.join(', ') ?? ''}
+                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={changeField('tags')}
+                    />
+                </div>
 
-            <div className="mb-4">
-                <label htmlFor="comments" className="block text-sm font-medium text-gray-700">
-                    Comments
-                </label>
-                <input
-                    type="text"
-                    name="comments"
-                    id="comments"
-                    value={movie?.comments}
-                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    onChange={changeField('comments')}
-                />
-            </div>
-        </form>);
+                <div className="mb-4">
+                    <label htmlFor="comments" className="block text-sm font-medium text-gray-700">
+                        Comments
+                    </label>
+                    <input
+                        type="text"
+                        name="comments"
+                        id="comments"
+                        value={movie?.comments ?? ''}
+                        className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={changeField('comments')}
+                    />
+                </div>
+            </form>
+        )
+            : <div>Is Loading...</div>
+    );
 }
 Movie.getInitialProps = props => props.query;
 Movie.propTypes = propTypes;
