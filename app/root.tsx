2a24f9fed016cad48ac09d54752e9a4ac62f96fc
steveoh <sgourley@utah.gov>
2022-07-13 15:23:58 -0600

    style: add tailwind
    Co-authored-by: Austin Haws <ahaws@utah.gov>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +7,18 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "React Guild Recordings",
+  description: "Recordings from React Guild presentations",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
 
 export default function App() {
   return (
